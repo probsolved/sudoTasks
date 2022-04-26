9f055131d302ee4825f0677489be0a74776df8a6
PRob-Solved <47791675+PRob-Solved@users.noreply.github.com>
2022-04-26 12:53:06 -0500

    Setup store data functionality

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { projectService } from "../projects/project.service";
 
 @Injectable({providedIn: 'root'})
 
 export class DataStorageService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private projectService: projectService) {}
+
+    storeProjects() {
+    const projects = this.projectService.getprojects();
+    this.http.put('https://sudotasks-df06e-default-rtdb.firebaseio.com/projects.json', projects).subscribe(response => {
+      console.log(response);
+    });
+  }
 }
