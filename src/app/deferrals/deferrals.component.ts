46f05e1b5c0240adf64a8fe3a7d8efc2917294a0
PRob-Solved <47791675+PRob-Solved@users.noreply.github.com>
2022-05-09 13:39:33 -0500

    make it better

--- src/app/deferrals/deferrals.component.ts
+++ src/app/deferrals/deferrals.component.ts
@@ -180,7 +180,7 @@ export class DeferralsComponent {
 
   handleEvent(action: string, event: CalendarEvent): void {
     this.modalData = { event, action };
-    this.modal.open(this.modalContent, { size: 'lg' });
+    // this.modal.open(this.modalContent, { size: 'lg' });
   }
 
   addEvent(): void {
